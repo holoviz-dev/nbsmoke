90b3b03a68b17aa2b3522d038e2b30c590fce209
C. E. Ball <ceball@gmail.com>
2018-02-16 11:06:32 +0000

    Trying to switch to pyviz standard tag format and understand travis docs...

--- _version.py
+++ _version.py
@@ -41,7 +41,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440-pre"
-    cfg.tag_prefix = ""
+    cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "nbsmoke-"
     cfg.versionfile_source = "_version.py"
     cfg.verbose = False
