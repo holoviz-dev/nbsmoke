c5670f69ce7da9a92092fca11c135142ab9922a4
C. E. Ball <ceball@gmail.com>
2019-06-12 12:06:16 +0100

    Simplify and improve magics handling (by using more ipython functions).

--- nbsmoke/lint/magics/__init__.py
+++ nbsmoke/lint/magics/__init__.py
@@ -8,15 +8,11 @@ info.
 
 """
 
-# TODO: instead of this hacky custom magics handling (parsing etc),
-# should just find and use ipython's own and then delete most of this
-# file. I.e. this is more of a proof of concept hack to see if
-# handling magics is useful, rather than something guaranteed to work.
-
 import warnings
 import ast
 
 import nbformat
+from nbconvert.filters.strings import ipython2python
 
 # these will be ignored (i.e. will not be present in python that gets
 # lint checked)
@@ -25,6 +21,9 @@ IGNORED_MAGICS = ['output','timer','matplotlib','env','load','history','writefil
 
 # "magic xyz..." will be replaced by "xyz..."
 SIMPLE_MAGICS = ['time','timeit','prun']
+
+# TODO: there are lots more inbuilt magics we should probably
+# handle. See get_ipython().magics_manager.registry.
 
 # placeholder for unavailable imports
 class _Unavailable:
@@ -53,33 +52,36 @@ def insert_get_ipython(nb):
         nb['cells'].insert(0,get_ipython_cell)
 
 
-def ipython_to_python_for_flake_checks(py):
+def ipython_to_python_for_flake_checks(ipy):
     """Given some ipython, return python code suitable for flake checking.
 
     Regular python (non-magic) lines will be left alone.
 
-    Certain magics will just be ignored (omitted from generated
-    python), e.g. "output" (see IGNORED_MAGICS).
+    Zero-arg magics will be ignored (omitted from generated python).
 
-    Certain single-argument magics that wrap valid python will be
-    processed in a simple way: (e.g. "time fn()" will be replaced by
-    "fn()"; see SIMPLE_MAGICS).
+    Certain other magics will also be ignored (omitted from generated
+    python), e.g. "matplotlib" (see IGNORED_MAGICS).
+
+    Certain single-argument magics that wrap some regular python will
+    be unwrapped: (e.g. "%time fn()" will be replaced by "fn()"; see
+    SIMPLE_MAGICS).
 
     Optional "magic handlers" can be registered to deal with
     custom/third-party magics (see other_magic_handlers).
 
-    If a magic is encountered that wasn't handled by any of the above,
-    it'll be omitted with a warning.
+    If a magic is encountered that wasn't handled in any of the above
+    ways, it'll be omitted, but with a warning.
     """
-    newlines = []
-    lines = py.split('\n')
-    for line in lines:
-        magic_parser = _get_parser(line)
-        newlines.append(_process_magics(magic_parser))
-        for l in magic_parser.additional_lines:
-            newlines.append(_process_magics(_get_parser(l)))
-    return "\n".join(newlines)
+    ipy_lines = ipy.split('\n') # this is valid for notebooks (json) - i.e. can't be other line endings?
+    py_lines = []
+    _transform_ipy_to_py(ipy_lines, py_lines)
+    return "\n".join(py_lines)
 
+def _transform_ipy_to_py(lines_in, lines_out):
+    for line in lines_in:
+        magic_parser = _get_parser(line)
+        lines_out.append(_process_magics(magic_parser))
+        _transform_ipy_to_py(magic_parser.additional_lines, lines_out)
 
 def _get_parser(line):
     parser = None
@@ -88,12 +90,14 @@ def _get_parser(line):
             parser = cls
     return parser(line)
 
-
 def _process_magics(magic):
     if isinstance(magic, NotMagic):
         content = magic.python
-    elif magic.skippable or magic.name in IGNORED_MAGICS:
-        content = 'pass # was: %s'%magic.line
+    elif magic.skippable:
+        # this is really just to help with debugging
+        content = 'pass # was line: %s'%magic.line
+    elif magic.name in IGNORED_MAGICS:
+        content = 'pass # was %s: %s %s'%(magic.__class__.__name__,magic.name,magic.python)
     elif magic.name in SIMPLE_MAGICS:
         content = magic.python
     elif magic.name in other_magic_handlers:
@@ -103,19 +107,18 @@ def _process_magics(magic):
             raise Exception("nbsmoke can't process the following '%s' magic without additional dependencies:\n  %s\n. Error was:\n  %s"%(magic.name, magic.line, handler.e.msg))
         content = handler(magic.python)
     else:
-        # TODO: Allow people to ignore stuff per project? E.g. "If
-        # specific to the current project, pass
-        # --nbsmoke-lint-magics-to-ignore=... or set
-        # nbsmoke_lint_magics_to_ignore in ini file... or more
-        # sophisticated additional magics handling can be added via
-        # other_magic_handlers...".
+        # TODO: Allow people to ignore stuff per project.  E.g. allow
+        # to add to SIMPLE_MAGICS, IGNORED_MAGICS,
+        # other_magic_handlers via command-line arg and ini file. And
+        # point to that mechanism in this warning.
         warnings.warn("nbsmoke doesn't know how to process the '%s' magic and has ignored it. Line:\n%s\nPlease file an issue at github.com/pyviz/nbsmoke/issues if it should be processed, or if it should be silently ignored."%(magic.name,magic.line))
         content = 'pass # was: %s'%magic.line
 
     return magic.indent + content
 
 
-# what a miserable set of classes - at least enjoy deleting them :)
+# the following 'parser' classes could likely be simplified (there
+# were more of them, but I just cut a few out).
 
 # abstract
 class _HackyParser(object):
@@ -126,7 +129,9 @@ class _HackyParser(object):
     in the original notebook; cell magics will have been converted to
     something like "fn(magic..., lines)", where lines is the cell content.
 
-    type of magic (e.g. line) based on the start of the line - "start".
+    The type of magic (line or cell) is based on the start of the line
+    - see "start".
+
     """
     start = ''
     def __init__(self,line):
@@ -138,14 +143,14 @@ class _HackyParser(object):
 
           * name: the name of the magic (could be None).
 
-          * python: some valid bit of python hacked out of the line,
-            suitable for taking part in lint check.
+          * python: some valid bit of python taken from the original
+            line, suitable for taking part in lint check.
 
           * skippable: whether the python can be skipped from linting
             (because it's empty).
 
-          * additional_lines: some magics (e.g. CompleteCell) can
-            contain other lines that will themselves need processing.
+          * additional_lines: some magics (e.g. Cell) can contain
+            other lines that will themselves need processing.
 
         """
         self.line = line
@@ -172,20 +177,15 @@ class _HackyParser(object):
     def _get_python(cls, pre_line):
         return pre_line
 
-    @staticmethod
-    def _get_additional_lines(pre_line):
+    def _get_additional_lines(self, pre_line):
         return []
 
 class NotMagic(_HackyParser):
-    """
-    Line wasn't a magic fn
-    """
+    """Line wasn't a magic"""
     pass
 
 # abstract
 class _Magic(_HackyParser):
-    # Magic that was converted by nbconvert, so will be something like
-    # get_ipython.run_cell_magic(magic, ...)."
 
     @staticmethod
     def _parse(x,i):
@@ -199,55 +199,20 @@ class _Magic(_HackyParser):
     def _get_python(cls, pre_line):
         return cls._parse(pre_line, 1)
 
-
-# abstract
-class _RawMagic(_Magic):
-    # Magic that was not converted by nbconvert, so will be something
-    # like %%magic ...
-
-    @classmethod
-    def _get_name(cls, line):
-        return cls._parse(line, 0)
-
-    @classmethod
-    def _get_python(cls, line):
-        return  cls._parse(line, 1)
-
-    @classmethod
-    def _parse(cls, x, i):
-        bits = x.strip()[len(cls.start)::].split(" ", 1)
-        if len(bits) == 1:
-            return ''
-        else:
-            return bits[i]
-
-# abstract
-class _CellMagic(_Magic):
-    pass
-
-class CompleteCellMagic(_CellMagic):
-    """
-    First cell magic in cell; was converted to fn by nbconvert.
-    """
+class CellMagic(_Magic):
+    """cell magic that was converted to fn by nbconvert"""
     start = 'get_ipython().run_cell_magic('
-    @classmethod
-    def _get_additional_lines(cls, pre_line):
-        return cls._parse(pre_line, 2).splitlines()
+
+    def _get_additional_lines(self, pre_line):
+        remaining_lines = self._parse(pre_line, 2).splitlines()
+        return ipython2python("\n".join(remaining_lines)).splitlines()
 
 class LineMagic(_Magic):
-    """line magic (was converted to fn by nbconvert)"""
+    """line magic that was converted to fn by nbconvert"""
     start = 'get_ipython().run_line_magic('
 
-class LineRawMagic(_RawMagic):
-    """line magic inside cell magic"""
-    start = "%"
-
-class CellRawMagic(_RawMagic, _CellMagic):
-    """second or subsequent cell magic"""
-    start = "%%"
-
 parsers = {}
-for cls in (NotMagic, CompleteCellMagic, LineMagic, LineRawMagic, CellRawMagic):
+for cls in (NotMagic, CellMagic, LineMagic):
     assert cls.start not in parsers
     parsers[cls.start] = cls
 
