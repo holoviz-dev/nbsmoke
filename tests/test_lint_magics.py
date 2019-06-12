04f8412406d38eef9b193e48687d218bfc9aced6
C. E. Ball <ceball@gmail.com>
2019-06-12 12:11:44 +0100

    Added test that failed before the previous commit (misdetection of magics). Also added test that cell magic skipping is working, since such a test was missing.

--- tests/test_lint_magics.py
+++ tests/test_lint_magics.py
@@ -130,6 +130,50 @@ nb_cell_and_line_magics = u'''
 }
 '''
 
+# note: % escaped as % subst
+nb_cell_skipped_and_line_magics = u'''
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import holoviews as hv\\n",
+    "import os\\n",
+    "from holoviews.operation.datashader import datashade"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "%%%%matplotlib inline\\n",
+    "%%%%capture --no-stderr\\n",
+    "%%%%html --isolated\\n",
+    "%%time %(dim_name)s = hv.Dimension('A')\\n",
+    "bdim = hv.Dimension('B')\\n",
+    "paths = [os.path.join('data', f) for f in ['a','b']]\\n",
+    "hv.Layout([datashade(hv.Curve(p, kdims=[adim], vdims=[bdim])) for p in paths]).cols(2)"
+   ]
+  }
+ ],
+ "metadata": {
+  "language_info": {
+   "name": "python",
+   "pygments_lexer": "ipython3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
+'''
+
+
+
 # note: % escaped
 nb_line_magic = u'''
 {
@@ -280,6 +324,45 @@ nb_lint_cell_line_magic_indent =  u'''
 }
 '''
 
+nb_lint_is_it_magic =  u'''
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "%%matplotlib inline\\n",
+    "import hv\\n",
+    "for m in [1,2,3]:\\n",
+    "    def cb(metric=m):\\n",
+    "        return hv.Curve(\\n",
+    "            '%s %s'\\n",
+    "            % (metric, 1))\\n",
+    "    def cb2(metric=m):\\n",
+    "        return hv.Curve(\\n",
+    "            '%sad'\\n",
+    "            % metric )\\n",
+    "    def cb3(metric=m):\\n",
+    "        return hv.Curve(\\n",
+    "            '%sad'\\n",
+    "            %metric )"
+   ]
+  }
+ ],
+ "metadata": {
+  "language_info": {
+   "name": "python",
+   "pygments_lexer": "ipython3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
+'''
+
+
 
 def test_lint_magics_nowarn(testdir):
     # there should be no warnings for a variety of magics
@@ -311,6 +394,14 @@ def test_lint_line_magics_with_cell_magics_good(testdir):
 
 def test_lint_line_magics_with_cell_magics_bad(testdir):
     testdir.makefile('.ipynb', testing123=nb_cell_and_line_magics%{'dim_name':'bad_name'})
+    result = testdir.runpytest('--nbsmoke-lint','-v')
+    result.stdout.re_match_lines_random(
+        [".*undefined name 'adim'$"])
+    assert result.ret == 1
+
+def test_lint_line_magics_with_skipped_cell_magics_bad(testdir):
+    # when omitting cell magic, make sure rest of cell is not also omitted
+    testdir.makefile('.ipynb', testing123=nb_cell_skipped_and_line_magics%{'dim_name':'bad_name'})
     result = testdir.runpytest('--nbsmoke-lint','-v')
     result.stdout.re_match_lines_random(
         [".*undefined name 'adim'$"])
@@ -359,4 +450,20 @@ def test_lint_cell_line_magics_indent(testdir):
     testdir.makefile('.ipynb', testing123=nb_lint_cell_line_magic_indent)
     result = testdir.runpytest('--nbsmoke-lint','-v')
     assert result.ret == 0
+    assert 'warnings' not in result.parseoutcomes()
+
+
+def test_magics_detection(testdir):
+    # if you were to parse ipython stuff line by line, you might mistake
+    # things for magics, e.g.
+    #   'some %s'
+    #   %variable
+    testdir.makefile('.ipynb', testing123=nb_lint_is_it_magic)
+    result = testdir.runpytest('--nbsmoke-lint','-v')
+    # if falsely detecting magic, the "pass" that gets inserted for
+    # the unknown magic would be a syntax error
+    assert result.ret == 0
+    # also, the following should not be present (would be warned about
+    # as unknown magic)
+    #   get_ipython().run_line_magic('metric', ')')
     assert 'warnings' not in result.parseoutcomes()
