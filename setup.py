e774ed8e1d5a308cb48338c3cf3bc2f32190d6db
C. E. Ball <ceball@gmail.com>
2019-09-01 14:40:59 +0100

    guess at some parentheses for py2

--- setup.py
+++ setup.py
@@ -59,8 +59,8 @@ setup_args = dict(
     # right? (but if so, how do I specify it's the runtime python version
     # I'm talking aobut, not the buildtime python version?)
     # Also - not sure exactly what is required now
-    ] + ['ipykernel'] if (sys.version_info[0]>=3 and sys.version_info[1]>4) else ['ipykernel <5'] + \
-        ['tornado ==4.1'] if sys.version_info[0]<3 else [], # bug in tornado - still there?
+    ] + (['ipykernel'] if (sys.version_info[0]>=3 and sys.version_info[1]>4) else ['ipykernel <5']) + \
+        (['tornado ==4.1'] if sys.version_info[0]<3 else []), # bug in tornado - still there?
     
     entry_points={
         'pytest11': [
