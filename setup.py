497d8464d316a52f951f547b10eab7f3e0100d89
Chris B <ceball@users.sf.net>
2019-10-26 22:47:49 +0100

    Remove files that are unused (test-notebooks) or do not need to exist (conda recipe, dodo.py). Move holoviews to optional dependency.

--- setup.py
+++ setup.py
@@ -52,14 +52,14 @@ setup_args = dict(
         ########## "verify" stuff
         'requests',
         'beautifulsoup4',
-        ########## "extra magics" support
-        # TODO: will be removing the hv stuff
-        'holoviews'
     # TODO: I think the decision was to go with python/setup.py for this stuff,
     # right? (but if so, how do I specify it's the runtime python version
     # I'm talking aobut, not the buildtime python version?)
     # Also - not sure exactly what is required now
     ] + (['ipykernel'] if (sys.version_info[0]>=3 and sys.version_info[1]>4) else ['ipykernel <5']),
+    extras_require = {
+        'holoviews-magics': holoviews,
+    }
     entry_points={
         'pytest11': [
             'nbsmoke = nbsmoke',
